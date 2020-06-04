221b50ef29574ad838894d7b324194463a70d0e1
andreevvikenty <66430627+andreevvikenty@users.noreply.github.com>
2020-06-04 18:33:49 +0300

    Add files via upload

---
+++ l1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace split_join
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string d = Console.ReadLine();
+            string[] words = d.Split(' ');//расщепляет на слова
+            Console.WriteLine("reversed:");
+            Array.Reverse(words);
+
+            for (int i = 0; i < words.Length; i++)
+
+
+            {
+
+
+                Console.Write(words[i] + " ");
+
+
+            }
+
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
